2167264b28d24b29099bd8fd7fa39963bbd9f038
Phạm Thanh Tường <tuongpt12503@gmail.com>
2023-10-29 13:05:57 +0700

    update management page

--- personal_schedule_management/lib/features/pages/managemet_page.dart
+++ personal_schedule_management/lib/features/pages/managemet_page.dart
@@ -1,15 +1,231 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:personal_schedule_management/config/text_styles/app_text_style.dart';
+import 'package:personal_schedule_management/config/theme/app_theme.dart';
 
-class ManagementPage extends StatelessWidget {
+class ManagementPage extends StatefulWidget {
   const ManagementPage({super.key});
 
   @override
+  State<ManagementPage> createState() => _ManagementPageState();
+}
+
+class _ManagementPageState extends State<ManagementPage> {
+  String selectedCategory = 'Tất cả';
+  bool isTruocExpanded = false;
+  bool isTuongLaiExpanded = false;
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    return Center(
-      child: FilledButton(
-        child: Text('This is management page'),
-        onPressed: () {},
+    Size size = MediaQuery.of(context).size;
+    return Scaffold(
+      body: Stack(
+        alignment: Alignment.topLeft,
+        children: [
+          Container(
+            height: size.height,
+            width: size.width,
+            padding: EdgeInsets.all(24),
+            child: Column(
+              children: [
+                Row(
+                  children: [
+                    InkWell(
+                      child: Container(
+                        alignment: Alignment.center,
+                        height: 35,
+                        width: (size.width - 108) / 3,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          color: selectedCategory == 'Tất cả'
+                              ? ColorPalette.onselectedColor
+                              : lightColorScheme.tertiaryContainer,
+                        ),
+                        child: Text(
+                          'Tất cả',
+                          style: AppTextStyle.h3.copyWith(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          selectedCategory =
+                              'Tất cả'; // Cập nhật biến khi được chọn
+                        });
+                      },
+                    ),
+                    SizedBox(width: 10),
+                    InkWell(
+                      child: Container(
+                        height: 35,
+                        alignment: Alignment.center,
+                        width: (size.width - 108) / 3,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          color: selectedCategory == 'Công việc'
+                              ? ColorPalette.onselectedColor
+                              : lightColorScheme.tertiaryContainer,
+                        ),
+                        child: Text(
+                          'Công việc',
+                          style: AppTextStyle.h3.copyWith(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          selectedCategory = 'Công việc';
+                        });
+                      },
+                    ),
+                    SizedBox(width: 10),
+                    InkWell(
+                      child: Container(
+                        height: 35,
+                        alignment: Alignment.center,
+                        width: (size.width - 108) / 3,
+                        decoration: BoxDecoration(
+                          border: Border.all(width: 1),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(10),
+                          ),
+                          color: selectedCategory == 'Cá nhân'
+                              ? ColorPalette.onselectedColor
+                              : lightColorScheme.tertiaryContainer,
+                        ),
+                        child: Text(
+                          'Cá nhân',
+                          style: AppTextStyle.h3.copyWith(
+                            fontSize: 15,
+                          ),
+                        ),
+                      ),
+                      onTap: () {
+                        setState(() {
+                          selectedCategory = 'Cá nhân';
+                        });
+                      },
+                    ),
+                    SizedBox(width: 10),
+                    Container(
+                      width: 30,
+                      alignment: Alignment.centerRight,
+                      child: PopupMenuButton<String>(
+                        padding: const EdgeInsets.all(0),
+                        icon: Icon(Icons.more_vert),
+                        onSelected: (value) {
+                          if (value == 'mana') {
+                          } else if (value == 'search') {
+                          } else if (value == 'sort') {
+                          } else if (value == 'in') {}
+                        },
+                        itemBuilder: (BuildContext context) =>
+                            <PopupMenuEntry<String>>[
+                          const PopupMenuItem<String>(
+                            value: 'mana',
+                            child: Text('Quản lý Danh mục'),
+                          ),
+                          const PopupMenuItem<String>(
+                            value: 'search',
+                            child: Text('Tìm kiếm'),
+                          ),
+                          const PopupMenuItem<String>(
+                            value: 'sort',
+                            child: Text('Sắp xếp công việc'),
+                          ),
+                          const PopupMenuItem<String>(
+                            value: 'in',
+                            child: Text('In'),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      isTruocExpanded = !isTruocExpanded;
+                    });
+                  },
+                  child: Container(
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Trước',
+                          style: AppTextStyle.h3.copyWith(
+                            fontSize: 18,
+                          ),
+                        ),
+                        Icon(isTruocExpanded
+                            ? Icons.arrow_drop_up
+                            : Icons.arrow_drop_down),
+                      ],
+                    ),
+                  ),
+                ),
+                if (isTruocExpanded)
+                  Container(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text('Mục 1'),
+                        ),
+                        ListTile(
+                          title: Text('Mục 2'),
+                        ),
+                      ],
+                    ),
+                  ),
+                InkWell(
+                  onTap: () {
+                    setState(() {
+                      isTuongLaiExpanded = !isTuongLaiExpanded;
+                    });
+                  },
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Tương lai',
+                        style: AppTextStyle.h3.copyWith(
+                          fontSize: 18,
+                        ),
+                      ),
+                      Icon(isTuongLaiExpanded
+                          ? Icons.arrow_drop_up
+                          : Icons.arrow_drop_down),
+                    ],
+                  ),
+                ),
+                if (isTuongLaiExpanded)
+                  Container(
+                    child: Column(
+                      children: [
+                        ListTile(
+                          title: Text('Mục 1'),
+                        ),
+                        ListTile(
+                          title: Text('Mục 2'),
+                        ),
+                        // Thêm các mục khác ở đây
+                      ],
+                    ),
+                  ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
