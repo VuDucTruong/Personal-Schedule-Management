ad836ffc26493af4bf2e0b719258e53118b8dad5
VuDucTruong <21520507@gm.uit.edu.vn>
2023-11-18 15:52:42 +0700

    create UI sync page

--- personal_schedule_management/lib/config/routes/routes.dart
+++ personal_schedule_management/lib/config/routes/routes.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:personal_schedule_management/features/pages/sync_calendar_page.dart';
 import 'package:personal_schedule_management/features/pages/work_category_page.dart';
 
 class AppRoutes {
@@ -6,5 +7,10 @@ class AppRoutes {
       context,
       MaterialPageRoute(
         builder: (context) => WorkCategoryPage(),
+      ));
+  static void toSyncCalendarPage(BuildContext context) => Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => SyncCalendarPage(),
       ));
 }
