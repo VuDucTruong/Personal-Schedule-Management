71ec2ea2e39b87c3490112661ce16f37778be78d
HuynhDangQuang <quangptnk@gmail.com>
2023-12-03 16:53:16 +0700

    Add AppTheme, new constants and settings controller

--- personal_schedule_management/lib/core/constants/constants.dart
+++ personal_schedule_management/lib/core/constants/constants.dart
@@ -29,3 +29,27 @@ const Map<String, String> weekDaysMap = {
   'T7': 'SA',
   'CN': 'SU'
 };
+// shared preferences
+const String THEME = 'theme';
+abstract class AppTheme {
+  static const DEFAULT = 'Default';
+  static const ELECTRIC_VIOLET = 'Electric Violet';
+  static const HIPPIE_BLUE = 'Hippe Blue';
+  static const GREEN_FOREST = 'Green Forest';
+  static const SAKURA = 'Sakura';
+  static const RED_WINE = 'Red wine';
+  static const GOLD_SUNSET = 'Gold sunset';
+  static const BLUE_DELIGHT = 'Blue Delight';
+}
+
+const String DATE_FORMAT = 'date_format';
+abstract class AppDateFormat {
+  static const DAY_MONTH_YEAR = 'dd/MM/yyyy';
+  static const MONTH_DAY_YEAR = 'MM/dd/yyyy';
+  static const YEAR_MONTH_DAY = 'yyyy/MM/dd';
+  static const YEAR_DAY_MONTH = 'yyyy/dd/MM';
+}
+
+const String TIME_24H_FORMAT = 'time_24';
+const String WEATHER = 'weather';
+
--- personal_schedule_management/lib/features/controller/settings_controller.dart
+++ personal_schedule_management/lib/features/controller/settings_controller.dart
@@ -1,9 +1,84 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:personal_schedule_management/config/theme/app_theme.dart';
+import 'package:personal_schedule_management/core/constants/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingsController {
   User? get currentUser => FirebaseAuth.instance.currentUser;
 
   Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
+  }
+
+  Future<void> SetTime24hFormatSetting(bool result) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(TIME_24H_FORMAT, result);
+  }
+
+  Future<bool?> GetTime24hFormatSetting() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(TIME_24H_FORMAT);
+  }
+
+  Future<void> SetWeatherSetting(bool result) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setBool(WEATHER, result);
+  }
+
+  Future<bool?> GetWeatherSetting() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getBool(WEATHER);
+  }
+
+  Future<void> SetDateFormat(format) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(DATE_FORMAT, format);
+  }
+
+  Future<String?> GetDateFormat() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(DATE_FORMAT);
+  }
+
+  Future<void> SetAppTheme(format) async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.setString(THEME, format);
+  }
+
+  Future<String?> GetAppTheme() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    return prefs.getString(THEME);
+  }
+
+  void LoadAppTheme(AppThemeName) {
+    switch (AppThemeName)
+    {
+      case AppTheme.DEFAULT:
+        AppThemeSet_Default();
+        break;
+      case AppTheme.ELECTRIC_VIOLET:
+        AppThemeSet_ElectricViolet();
+        break;
+      case AppTheme.BLUE_DELIGHT:
+        AppThemeSet_BlueDelight();
+        break;
+      case AppTheme.HIPPIE_BLUE:
+        AppThemeSet_HippieBlue();
+        break;
+      case AppTheme.GOLD_SUNSET:
+        AppThemeSet_GoldSunset();
+        break;
+      case AppTheme.GREEN_FOREST:
+        AppThemeSet_GreenForest();
+        break;
+      case AppTheme.RED_WINE:
+        AppThemeSet_RedWine();
+        break;
+      case AppTheme.SAKURA:
+        AppThemeSet_Sakura();
+        break;
+      default:
+        AppThemeSet_Default();
+    }
   }
 }
