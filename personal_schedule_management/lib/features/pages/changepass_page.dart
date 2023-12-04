0aaff0ce2c12c1f16e85ea03b9b01832f2eeeacf
HuynhDangQuang <quangptnk@gmail.com>
2023-12-04 22:17:32 +0700

    Update changepass_page.dart
    Fixed bug: "First pressing on Save button is not working"

--- personal_schedule_management/lib/features/pages/changepass_page.dart
+++ personal_schedule_management/lib/features/pages/changepass_page.dart
@@ -78,8 +78,11 @@ class _ChangePassPageState extends State<ChangePassPage> {
   }
 
   void _ChangePassButton(context) async {
+    _OldPasswordFocus.unfocus();
     _PasswordFocus.unfocus();
     _RePasswordFocus.unfocus();
+    _PasswordValidateText = _PasswordValidating(_PasswordController.value.text);
+    _RePasswordValidateText = _RePasswordValidating(_RePasswordController.value.text);
     if (_PasswordCorrect && _RePasswordCorrect) {
       // do something
       bool? result = true;
