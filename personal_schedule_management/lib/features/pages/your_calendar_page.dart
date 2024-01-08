cfb517d5c9e8ec6b7065f53082a8f49d1396821a
VuDucTruong <21520507@gm.uit.edu.vn>
2024-01-08 22:24:05 +0700

    Update your_calendar_page.dart

--- personal_schedule_management/lib/features/pages/your_calendar_page.dart
+++ personal_schedule_management/lib/features/pages/your_calendar_page.dart
@@ -23,7 +23,7 @@ class _YourCalendarPageState extends State<YourCalendarPage> {
   bool _modified = false;
 
   Future<void> _close(BuildContext context) async {
-    if (_modified){
+    if (_modified) {
       CalendarPageController.isSyncCalendarModified = true;
     }
   }
@@ -31,150 +31,166 @@ class _YourCalendarPageState extends State<YourCalendarPage> {
   @override
   void initState() {
     super.initState();
-    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
-      yourCalendarController.getAllGoogleAccounts().whenComplete(() {
-        setState(() {});
-      });
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    const buttonRatio = 0.9;
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      yourCalendarController.getAllGoogleAccounts().whenComplete(() {
+        if (mounted) {
+          setState(() {});
+        }
+      });
+    });
     return SafeArea(
       child: Builder(builder: (context) {
         return PopScope(
-          onPopInvoked: (bool didPoop) async {
-            if (didPoop){
-            await _close(context);
-            }
-          },
-          child: Scaffold(
-            resizeToAvoidBottomInset: false,
-            appBar: AppBar(
-              centerTitle: false,
-              leading: IconButton(
-                  onPressed: () async {
-                    Navigator.of(context).pop();
-                  },
-                  icon: Icon(Icons.arrow_back,
-                      size: 40, color: Theme.of(context).colorScheme.primary)),
-              title: Text('Lịch của bạn',
-                  style: Theme.of(context).textTheme.headlineSmall!.copyWith(
-                      color: Theme.of(context).colorScheme.primary,
-                      fontWeight: FontWeight.bold)),
-            ),
-            body: SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  SizedBox(height: 16),
-                  Card(
-                      elevation: 5,
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Container(
-                              margin: const EdgeInsets.only(
-                                  left: 4.0, top: 4.0, right: 4.0, bottom: 4.0),
-                              child: InkWell(
-                                  child: ListTile(
-                                    title: Row(
-                                      children: [
-                                        ClipRRect(
-                                          borderRadius:
-                                              BorderRadius.circular(5.0), //or 15.0
-                                          child: Container(
-                                            height: 50,
-                                            width: 50,
-                                            color: Colors.white,
-                                            child: Image.asset(
-                                                'assets/image/google_image.png'),
-                                          ),
-                                        ),
-                                        const SizedBox(
-                                          width: 16,
-                                        ),
-                                        Text('Tài khoản Google',
-                                            style: TextStyle(
-                                                fontSize: 18,
-                                                color: Theme.of(context)
-                                                    .colorScheme
-                                                    .onPrimaryContainer,
-                                                fontWeight: FontWeight.bold)),
-                                      ],
-                                    ),
-                                    trailing: Switch(
-                                      value: yourCalendarController.isSync,
-                                      onChanged: (value) async {
-                                        if (await yourCalendarController
-                                            .changeSync(value)) {
-                                          _modified = true;
-                                          setState(() {});
-                                        }
-                                      },
-                                    ),
-                                  )
-                              )
-                          ),
-                          Visibility(
-                            visible: yourCalendarController.isSync,
-                            child: ListView.builder(
-                              primary: false,
-                              shrinkWrap: true,
-                              itemCount:
-                                  yourCalendarController.accountList.length,
-                              itemBuilder: (context, index) {
-                                return ListTile(
-                                  title: Text(
-                                    yourCalendarController.accountList[index].key,
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 16),
-                                  ),
-                                  subtitle: Column(
+            onPopInvoked: (bool didPoop) async {
+              if (didPoop) {
+                await _close(context);
+              }
+            },
+            child: Scaffold(
+              resizeToAvoidBottomInset: false,
+              appBar: AppBar(
+                centerTitle: false,
+                leading: IconButton(
+                    onPressed: () async {
+                      Navigator.of(context).pop();
+                    },
+                    icon: Icon(Icons.arrow_back,
+                        size: 40,
+                        color: Theme.of(context).colorScheme.primary)),
+                title: Text('Lịch của bạn',
+                    style: Theme.of(context).textTheme.headlineSmall!.copyWith(
+                        color: Theme.of(context).colorScheme.primary,
+                        fontWeight: FontWeight.bold)),
+              ),
+              body: SingleChildScrollView(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(height: 16),
+                    Card(
+                        elevation: 5,
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Container(
+                                margin: const EdgeInsets.only(
+                                    left: 4.0,
+                                    top: 4.0,
+                                    right: 4.0,
+                                    bottom: 4.0),
+                                child: InkWell(
+                                    child: ListTile(
+                                  title: Row(
                                     children: [
-                                      ...yourCalendarController
-                                          .accountList[index].value
-                                          .map((e) => CheckboxListTile(
-                                                value: yourCalendarController
-                                                    .accountMap[yourCalendarController.accountList[index].key + '\n' + (e.name ?? '')],
-                                                onChanged: (value) {
-                                                  if (value != null) {
-                                                    if (!value) {
-                                                      yourCalendarController
-                                                          .addToBanList(e.id);
-                                                    } else {
-                                                      yourCalendarController
-                                                          .removeToBanList(e.id);
-                                                    }
-                                                    try {
-                                                      yourCalendarController
-                                                              .accountMap[
-                                                                yourCalendarController.accountList[index].key + '\n' + (e.name ?? '')] = value;
-                                                    } catch (e) {
-                                                      print(e);
-                                                    }
-                                                    _modified = true;
-                                                    setState(() {});
-                                                  }
-                                                },
-                                                title: Text(e.name ?? ''),
-                                              ))
+                                      ClipRRect(
+                                        borderRadius: BorderRadius.circular(
+                                            5.0), //or 15.0
+                                        child: Container(
+                                          height: 50,
+                                          width: 50,
+                                          color: Colors.white,
+                                          child: Image.asset(
+                                              'assets/image/google_image.png'),
+                                        ),
+                                      ),
+                                      const SizedBox(
+                                        width: 16,
+                                      ),
+                                      Text('Tài khoản Google',
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              color: Theme.of(context)
+                                                  .colorScheme
+                                                  .onPrimaryContainer,
+                                              fontWeight: FontWeight.bold)),
                                     ],
                                   ),
-                                );
-                              },
-                            ),
-                          )
-                        ],
-                      )),
-                ],
+                                  trailing: Switch(
+                                    value: yourCalendarController.isSync,
+                                    onChanged: (value) async {
+                                      if (await yourCalendarController
+                                          .changeSync(value)) {
+                                        _modified = true;
+                                        setState(() {});
+                                      }
+                                    },
+                                  ),
+                                ))),
+                            Visibility(
+                              visible: yourCalendarController.isSync,
+                              child: ListView.builder(
+                                primary: false,
+                                shrinkWrap: true,
+                                itemCount:
+                                    yourCalendarController.accountList.length,
+                                itemBuilder: (context, index) {
+                                  return ListTile(
+                                    title: Text(
+                                      yourCalendarController
+                                          .accountList[index].key,
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          fontSize: 16),
+                                    ),
+                                    subtitle: Column(
+                                      children: [
+                                        ...yourCalendarController
+                                            .accountList[index].value
+                                            .map((e) => CheckboxListTile(
+                                                  value: yourCalendarController
+                                                          .accountMap[
+                                                      yourCalendarController
+                                                              .accountList[
+                                                                  index]
+                                                              .key +
+                                                          '\n' +
+                                                          (e.name ?? '')],
+                                                  onChanged: (value) {
+                                                    if (value != null) {
+                                                      if (!value) {
+                                                        yourCalendarController
+                                                            .addToBanList(e.id);
+                                                      } else {
+                                                        yourCalendarController
+                                                            .removeToBanList(
+                                                                e.id);
+                                                      }
+                                                      try {
+                                                        yourCalendarController
+                                                                .accountMap[
+                                                            yourCalendarController
+                                                                    .accountList[
+                                                                        index]
+                                                                    .key +
+                                                                '\n' +
+                                                                (e.name ??
+                                                                    '')] = value;
+                                                      } catch (e) {
+                                                        print(e);
+                                                      }
+                                                      _modified = true;
+                                                      setState(() {});
+                                                    }
+                                                  },
+                                                  title: Text(e.name ?? ''),
+                                                ))
+                                      ],
+                                    ),
+                                  );
+                                },
+                              ),
+                            )
+                          ],
+                        )),
+                  ],
+                ),
               ),
-            ),
-          )
-        );
+            ));
       }),
     );
   }
