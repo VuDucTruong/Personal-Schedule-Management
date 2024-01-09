7428945cf3082bdc2383d47e6430a9b7864b6f4d
HuynhDangQuang <113320907+HuynhDangQuang@users.noreply.github.com>
2024-01-09 20:54:10 +0700

    Update your_calendar_page.dart

--- personal_schedule_management/lib/features/pages/your_calendar_page.dart
+++ personal_schedule_management/lib/features/pages/your_calendar_page.dart
@@ -31,10 +31,6 @@ class _YourCalendarPageState extends State<YourCalendarPage> {
   @override
   void initState() {
     super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
     WidgetsBinding.instance.addPostFrameCallback((_) {
       yourCalendarController.getAllGoogleAccounts().whenComplete(() {
         if (mounted) {
@@ -42,6 +38,10 @@ class _YourCalendarPageState extends State<YourCalendarPage> {
         }
       });
     });
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return SafeArea(
       child: Builder(builder: (context) {
         return PopScope(
